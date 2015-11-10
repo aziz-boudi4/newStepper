e11eb268a53029b96102c9501fa507964ff5a3ab
Aziz Boudi <azicode4@gmail.com>
2015-11-10 13:30:07 -0800

    - Got rid of the dec function ( not used ). - Implemented requireGestureToFail(swipeGesture) so that if the pan gesture is triggered, the swipe gesture stops and vice versa. - Got rid of unused code and comments.

--- newStepper/ViewController.swift
+++ newStepper/ViewController.swift
@@ -11,6 +11,8 @@
 import UIKit
 import QuartzCore
 
+
+
 class ViewController: UIViewController {
 
   // IBActions
@@ -66,15 +68,7 @@ class ViewController: UIViewController {
   }
 
   private func inc(n: Int) {
-
-      score = score + n
-  }
-  private func dec(n: Int) {
-    if score == 0 {
-      score = 0}
-    else {
-      score = score - n
-    }
+    score = score + n
   }
 
   override func viewDidLoad() {
@@ -83,8 +77,8 @@ class ViewController: UIViewController {
     arrowDown.alpha = 0
     arrowUp.alpha = 0
 
-    setupSwipeGestures()
-    setupPanGestures()
+    let swipeGestures = setupSwipeGestures()
+    setupPanGestures(swipeGestures)
     setupTapGesture()
 
 
@@ -92,7 +86,7 @@ class ViewController: UIViewController {
     circleView.layer.borderColor = UIColor.lightGrayColor().CGColor
   }
 
-  private func setupSwipeGestures() {
+  private func setupSwipeGestures() -> [UISwipeGestureRecognizer] {
     let swipeUp = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
     let swipeDown = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
 
@@ -101,6 +95,8 @@ class ViewController: UIViewController {
 
     circleView.addGestureRecognizer(swipeUp)
     circleView.addGestureRecognizer(swipeDown)
+
+    return [swipeUp, swipeDown]
 
 
   }
@@ -111,49 +107,42 @@ class ViewController: UIViewController {
   }
 
 
-  // add a pan gesture recognizer 
+  // add a pan gesture recognizer for multiple goals
 
-  private func setupPanGestures() {
+  private func setupPanGestures(swipeGestures: [UISwipeGestureRecognizer]) {
     let panGesture = UIPanGestureRecognizer(target: self, action: Selector("handleThePan:"))
+    for swipeGesture in swipeGestures {
+       panGesture.requireGestureRecognizerToFail(swipeGesture)
+    }
 
     circleView.addGestureRecognizer(panGesture)
-
-
-
   }
 
   func handleThePan(sender: UIPanGestureRecognizer) -> Void {
 
     if sender.isDown(circleView) == true {
       if score == 0 {
-        score = 0
+         score = 0
       } else {
-        inc(-1)
-      }
+         inc(-1)
+        }
     } else {
-      inc(+1)
+         inc(+1)
     }
   }
-
-
-  // both pan anf swipe at the same time
-
-//  func gestureRecognizer(UIPanGestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer UISwipeGestureRecognizer: UIGestureRecognizer) -> Bool {
-//    return true
-//  }
 
 
   // start animation of circle view when view is tapped
 
   func handleTap(sender: UITapGestureRecognizer) {
     UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.4, options: [.AllowUserInteraction , .CurveEaseInOut ], animations: {
-//    UIView.animateWithDuration(0.5, delay: 0.0, options: [  .AllowUserInteraction , .CurveEaseInOut  ] , animations: {
       self.circleView.transform = CGAffineTransformMakeScale(1.2, 1.2)
       self.circleView.layer.backgroundColor = UIColor(red: 167/255.0, green: 246/255.0, blue: 67/255.0, alpha: 1).CGColor
       self.arrowDown.alpha = 1
       self.arrowUp.alpha = 1
       },completion:nil)
 
+// Uncomment if you want the view to scale down after being tapped.
 //    UIView.animateWithDuration(0.7, delay: 0.7, options: [  .AllowUserInteraction , .CurveEaseInOut ] , animations: {
 //      self.circleView.transform = CGAffineTransformMakeScale(1, 1)
 //      },completion:nil)
@@ -161,7 +150,6 @@ class ViewController: UIViewController {
   }
 
   func handleSwipes(sender:UISwipeGestureRecognizer) {
-
 
     // up or down
     if sender.direction == .Down && score == 0  {
@@ -173,6 +161,7 @@ class ViewController: UIViewController {
       increment = 1
       offset = 10
       print("offset up :\(offset)")
+      // move the number too
       //label.center = CGPoint(x: label.center.x , y: label.center.y + offset)
 
     } else if  sender.direction == .Down  {
@@ -183,20 +172,15 @@ class ViewController: UIViewController {
 
     }
 
-    //let shadowAnimate = CABasicAnimation(keyPath:"shadowOpacity")
     // animate stuff with constraints
     inc(increment)
 
     UIView.animateWithDuration(0.2, animations: { _ in
       self.labelYConstraint.constant = self.offset
       self.view.layoutIfNeeded()
-
-      //self.circleView.layer.shadowRadius = 4
       self.circleView.layer.shadowOpacity = 1
-      //self.circleView.layer.shadowColor = UIColor(red: 167/255.0, green: 246/255.0, blue: 67/255.0, alpha: 1).CGColor
       self.circleView.layer.backgroundColor = UIColor(red: 167/255.0, green: 246/255.0, blue: 67/255.0, alpha: 1).CGColor
       self.circleView.layer.shadowOffset = CGSize.zero
-      //self.label.textColor = UIColor.blueColor()
       }) { _ in
 
         UIView.animateWithDuration(0.2, animations: { _ in
@@ -205,9 +189,6 @@ class ViewController: UIViewController {
           self.circleView.layer.shadowRadius = 1
           self.circleView.layer.shadowOpacity = 0.1
           self.circleView.layer.shadowColor = UIColor.clearColor().CGColor
-          //self.circleView.layer.backgroundColor = UIColor(red: 211/255.0, green: 211/255.0, blue: 211/255.0, alpha: 0.3).CGColor
-
-          //self.label.textColor = UIColor.blackColor()
         })
     }
   }
